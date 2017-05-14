855c4517c2d77befc1f35656ddfe8bb8aa39f2ce
Remco van Zuijlen <remco@zuijlen.eu>
2017-05-14 13:00:49 +0200

    more info

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 echo "Testing..."
 ls -l src/xlxd
 echo "tidy -e -q:"
-find . -name "*.html" -exec tidy -e -q {} \;
+find . -name "*.html" -print -exec tidy -e -q {} \;
 echo "php -l:"
-find . -name "*.php" -exec php -l {} \;
+find . -name "*.php" -print -exec php -l {} \;
 
