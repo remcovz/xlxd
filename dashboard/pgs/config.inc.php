b89ac78eeea31d00867b847d30792bce675fb60a
Remco PA4TW <remco@pa4tw.nl>
2018-12-26 14:41:30 +0100

    T module

--- dashboard/pgs/config.inc.php
+++ dashboard/pgs/config.inc.php
@@ -38,7 +38,7 @@ $PageOptions['ModuleNames'] = array();					// Module nomination
 $PageOptions['ModuleNames']['A']                     = 'Int.';
 $PageOptions['ModuleNames']['B']                     = 'Nederland';
 $PageOptions['ModuleNames']['C']                     = 'National';
-$PageOptions['ModuleNames']['D']                     = '';
+$PageOptions['ModuleNames']['T']                     = 'Peanut testing';
 
 $PageOptions['MetaDescription']                      = 'XLX is a D-Star Reflector System for Ham Radio Operators.';	// Meta Tag Values, usefull for Search Engine
 $PageOptions['MetaKeywords']                         = 'Ham Radio, D-Star, XReflector, XLX, XRF, DCS, REF, ';		// Meta Tag Values, usefull forSearch Engine
