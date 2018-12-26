6992e550c7196dc192f740a440cf1cf5ffddff72
Remco PA4TW <remco@pa4tw.nl>
2018-12-26 13:49:38 +0100

    merge with live version

--- dashboard/pgs/config.inc.php
+++ dashboard/pgs/config.inc.php
@@ -15,7 +15,7 @@ $CallingHome = array();
 $PageOptions = array();
 $VNStat      = array();
 
-$PageOptions['ContactEmail']                         = 'your_email';	// Support E-Mail address
+$PageOptions['ContactEmail']                         = 'remco@pa4tw.nl';	// Support E-Mail address
 
 $PageOptions['DashboardVersion']                     = '2.4.0';		// Dashboard Version
 
@@ -24,25 +24,25 @@ $PageOptions['PageRefreshDelay']                     = '10000';		// Page refresh
 
 $PageOptions['RepeatersPage'] = array();
 $PageOptions['RepeatersPage']['LimitTo']             = 99;		// Number of Repeaters to show
-$PageOptions['RepeatersPage']['IPModus']             = 'ShowFullIP';	// See possible options above
+$PageOptions['RepeatersPage']['IPModus']             = 'ShowLast2ByteOfIP';	// See possible options above
 $PageOptions['RepeatersPage']['MasqueradeCharacter'] = '*';		// Character used for  masquerade
 
 $PageOptions['PeerPage'] = array();
 $PageOptions['PeerPage']['LimitTo']                  = 99;		// Number of peers to show
-$PageOptions['PeerPage']['IPModus']                  = 'ShowFullIP';	// See possible options above
+$PageOptions['PeerPage']['IPModus']                  = 'ShowLast2ByteOfIP';	// See possible options above
 $PageOptions['PeerPage']['MasqueradeCharacter']      = '*';		// Character used for  masquerade
 
 $PageOptions['LastHeardPage']['LimitTo']             = 39;		// Number of stations to show
 
 $PageOptions['ModuleNames'] = array();					// Module nomination
 $PageOptions['ModuleNames']['A']                     = 'Int.';
-$PageOptions['ModuleNames']['B']                     = 'Regional';
+$PageOptions['ModuleNames']['B']                     = 'Nederland';
 $PageOptions['ModuleNames']['C']                     = 'National';
 $PageOptions['ModuleNames']['D']                     = '';
 
 $PageOptions['MetaDescription']                      = 'XLX is a D-Star Reflector System for Ham Radio Operators.';	// Meta Tag Values, usefull for Search Engine
 $PageOptions['MetaKeywords']                         = 'Ham Radio, D-Star, XReflector, XLX, XRF, DCS, REF, ';		// Meta Tag Values, usefull forSearch Engine
-$PageOptions['MetaAuthor']                           = 'LX1IQ';								// Meta Tag Values, usefull for Search Engine
+$PageOptions['MetaAuthor']                           = 'PA4TW';								// Meta Tag Values, usefull for Search Engine
 $PageOptions['MetaRevisit']                          = 'After 30 Days';							// Meta Tag Values, usefull for Search Engine
 $PageOptions['MetaRobots']                           = 'index,follow';							// Meta Tag Values, usefull for Search Engine
 
@@ -54,21 +54,21 @@ $PageOptions['CustomTXT']                            = '';					// custom text in
 $Service['PIDFile']                                  = '/var/log/xlxd.pid';
 $Service['XMLFile']                                  = '/var/log/xlxd.xml';
 
-$CallingHome['Active']                               = false;					// xlx phone home, true or false
-$CallingHome['MyDashBoardURL']                       = 'http://your_dashboard';			// dashboard url
+$CallingHome['Active']                               = true;					// xlx phone home, true or false
+$CallingHome['MyDashBoardURL']                       = 'http://pa4tw.nl/xlx';			// dashboard url
 $CallingHome['ServerURL']                            = 'http://xlxapi.rlx.lu/api.php';		// database server, do not change !!!!
 $CallingHome['PushDelay']                            = 600;					// push delay in seconds
-$CallingHome['Country']                              = "your_country";				// Country
-$CallingHome['Comment']                              = "your_comment";				// Comment. Max 100 character
-$CallingHome['HashFile']                             = "/tmp/callinghome.php";			// Make sure the apache user has read and write permissions in this folder.
+$CallingHome['Country']                              = "The Netherlands";				// Country
+$CallingHome['Comment']                              = "Experimental reflector run by PA4TW. Located in Amsterdam, NL.";				// Comment. Max 100 character
+$CallingHome['HashFile']                             = "/var/www/sites/ham.zuijlen.eu/tmp/callinghome.php";			// Make sure the apache user has read and write permissions in this folder.
 $CallingHome['LastCallHomefile']                     = "/tmp/lastcallhome.php";			// lastcallhome.php can remain in the tmp folder 
-$CallingHome['OverrideIPAddress']                    = "";					// Insert your IP address here. Leave blank for autodetection. No need to enter a fake address.
-$CallingHome['InterlinkFile']                        = "/xlxd/xlxd.interlink";			// Path to interlink file
+$CallingHome['OverrideIPAddress']                    = "194.109.192.236";					// Insert your IP address here. Leave blank for autodetection. No need to enter a fake address.
+$CallingHome['InterlinkFile']                        = "/home/dextra/xlxd/xlxd/xlxd.interlink";			// Path to interlink file
 
-$VNStat['Interfaces']                                = array();
-$VNStat['Interfaces'][0]['Name']                     = 'eth0';
-$VNStat['Interfaces'][0]['Address']                  = 'eth0';
-$VNStat['Binary']                                    = '/usr/bin/vnstat';
+//$VNStat['Interfaces']                                = array();
+//$VNStat['Interfaces'][0]['Name']                     = 'eth0';
+//$VNStat['Interfaces'][0]['Address']                  = 'eth0';
+//$VNStat['Binary']                                    = '/usr/bin/vnstat';
 
 /*   
 include an extra config file for people who dont like to mess with shipped config.ing.php   
