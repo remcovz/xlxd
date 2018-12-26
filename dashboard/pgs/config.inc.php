01ddec84b631823df744a00befd590f9417e6be3
Remco PA4TW <remco@pa4tw.nl>
2018-12-26 15:36:57 +0100

    dashboard config

--- dashboard/pgs/config.inc.php
+++ dashboard/pgs/config.inc.php
@@ -15,7 +15,7 @@ $CallingHome = array();
 $PageOptions = array();
 $VNStat      = array();
 
-$PageOptions['ContactEmail']                         = 'your_email';	// Support E-Mail address
+$PageOptions['ContactEmail']                         = 'remco@pa4tw.nl';		    // Support E-Mail address
 
 $PageOptions['DashboardVersion']                     = '2.4.0';		// Dashboard Version
 
@@ -38,37 +38,35 @@ $PageOptions['ModuleNames'] = array();					// Module nomination
 $PageOptions['ModuleNames']['A']                     = 'Int.';
 $PageOptions['ModuleNames']['B']                     = 'Regional';
 $PageOptions['ModuleNames']['C']                     = 'National';
-$PageOptions['ModuleNames']['D']                     = '';
+$PageOptions['ModuleNames']['T']                     = 'Peanut testing';
 
-$PageOptions['MetaDescription']                      = 'XLX is a D-Star Reflector System for Ham Radio Operators.';	// Meta Tag Values, usefull for Search Engine
-$PageOptions['MetaKeywords']                         = 'Ham Radio, D-Star, XReflector, XLX, XRF, DCS, REF, ';		// Meta Tag Values, usefull forSearch Engine
-$PageOptions['MetaAuthor']                           = 'LX1IQ';								// Meta Tag Values, usefull for Search Engine
-$PageOptions['MetaRevisit']                          = 'After 30 Days';							// Meta Tag Values, usefull for Search Engine
-$PageOptions['MetaRobots']                           = 'index,follow';							// Meta Tag Values, usefull for Search Engine
+$PageOptions['MetaDescription']                      = 'XLX is a D-Star Reflector System for Ham Radio Operators.';  // Meta Tag Values, usefull for Search Engine
+$PageOptions['MetaKeywords']                         = 'Ham Radio, D-Star, XReflector, XLX, XRF, DCS, REF, ';        // Meta Tag Values, usefull forSearch Engine
+$PageOptions['MetaAuthor']                           = 'PA4TW';                                                      // Meta Tag Values, usefull for Search Engine
+$PageOptions['MetaRevisit']                          = 'After 30 Days';                                              // Meta Tag Values, usefull for Search Engine
+$PageOptions['MetaRobots']                           = 'index,follow';                                               // Meta Tag Values, usefull for Search Engine
 
 $PageOptions['UserPage']['ShowFilter']               = true;								// Show Filter on Users page
 $PageOptions['Traffic']['Show']                      = false;								// Enable vnstat traffic statistics
 
-$PageOptions['CustomTXT']                            = '';					// custom text in your header   
+$PageOptions['CustomTXT']                            = 'Reachable over Hamnet only';                                    // custom text in your header
+$Service['PIDFile']                                  = '/var/log/xlxd3.pid';
+$Service['XMLFile']                                  = '/var/log/xlxd3.xml';
 
-$Service['PIDFile']                                  = '/var/log/xlxd.pid';
-$Service['XMLFile']                                  = '/var/log/xlxd.xml';
+$CallingHome['Active']                               = true;					               // xlx phone home, true or false
+$CallingHome['MyDashBoardURL']                       = 'http://pa4tw.nl/xlx087';			       // dashboard url
+$CallingHome['ServerURL']                            = 'http://xlxapi.rlx.lu/api.php';         // database server, do not change !!!!
+$CallingHome['PushDelay']                            = 600;  	                               // push delay in seconds
+$CallingHome['Country']                              = "The Netherlands";                         // Country
+$CallingHome['Comment']                              = "Hamnet facing reflector run by PA4TW. Located in Amsterdam, NL."; 				           // Comment. Max 100 character
+$CallingHome['HashFile']                             = "/var/www/sites/ham.zuijlen.eu/tmp/callinghome-087.php";                 // Make sure the apache user has read and write permissions in this folder.
+$CallingHome['OverrideIPAddress']                    = "44.137.36.209";                                     // Insert your IP address here. Leave blank for autodetection. No need to enter a fake address.
+$CallingHome['InterlinkFile']                        = "/home/dextra/xlx087/xlxd/xlxd.interlink";                 // Path to interlink file
 
-$CallingHome['Active']                               = false;					// xlx phone home, true or false
-$CallingHome['MyDashBoardURL']                       = 'http://your_dashboard';			// dashboard url
-$CallingHome['ServerURL']                            = 'http://xlxapi.rlx.lu/api.php';		// database server, do not change !!!!
-$CallingHome['PushDelay']                            = 600;					// push delay in seconds
-$CallingHome['Country']                              = "your_country";				// Country
-$CallingHome['Comment']                              = "your_comment";				// Comment. Max 100 character
-$CallingHome['HashFile']                             = "/tmp/callinghome.php";			// Make sure the apache user has read and write permissions in this folder.
-$CallingHome['LastCallHomefile']                     = "/tmp/lastcallhome.php";			// lastcallhome.php can remain in the tmp folder 
-$CallingHome['OverrideIPAddress']                    = "";					// Insert your IP address here. Leave blank for autodetection. No need to enter a fake address.
-$CallingHome['InterlinkFile']                        = "/xlxd/xlxd.interlink";			// Path to interlink file
-
-$VNStat['Interfaces']                                = array();
-$VNStat['Interfaces'][0]['Name']                     = 'eth0';
-$VNStat['Interfaces'][0]['Address']                  = 'eth0';
-$VNStat['Binary']                                    = '/usr/bin/vnstat';
+//VNStat['Interfaces']                                = array();
+//$VNStat['Interfaces'][0]['Name']                     = 'eth0';
+//$VNStat['Interfaces'][0]['Address']                  = 'eth0';
+//$VNStat['Binary']                                    = '/usr/bin/vnstat';
 
 /*   
 include an extra config file for people who dont like to mess with shipped config.ing.php   
